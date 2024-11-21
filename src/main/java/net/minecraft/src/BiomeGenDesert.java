2ca3d5bc9e8fd76dac942b0237c05ba4dde8042d
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2024-11-21 13:56:28 -0500

    Update BiomeGenDesert.java

--- src/main/java/net/minecraft/src/BiomeGenDesert.java
+++ src/main/java/net/minecraft/src/BiomeGenDesert.java
@@ -2,7 +2,7 @@ package net.minecraft.src;
 
 public class BiomeGenDesert extends BiomeGenBase {
   public BiomeGenDesert() {
-		this.spawnableCreatureList.add(new SpawnListEntry(EntityGiant.class, 2));
+		this.spawnableCreatureList.add(new SpawnListEntry(EntityGiantZombie.class, 1));
 	}
 
 }
