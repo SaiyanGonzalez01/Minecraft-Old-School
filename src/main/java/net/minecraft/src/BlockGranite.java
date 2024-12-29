7e8c8ff880a2e02a080cceca4ac275b1afe60fe1
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2024-12-29 18:14:45 -0500

    Create BlockGranite.java

---
+++ src/main/java/net/minecraft/src/BlockGranite.java
@@ -0,0 +1,11 @@
+package net.minecraft.src;
+
+public class BlockGranite extends Block {
+	public BlockGranite(int var1, int var2) {
+		super(var1, var2, Material.rock);
+	}
+
+	public int idDropped(int var1, Random var2) {
+		return Block.granite.blockID;
+	}
+}
