951d788acefa3e0ed0b329d9a92518e3fe065f06
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2024-12-29 16:50:14 -0500

    Update Block.java

--- src/main/java/net/minecraft/src/Block.java
+++ src/main/java/net/minecraft/src/Block.java
@@ -116,8 +116,8 @@ public class Block {
 	public static final Block redstoneRepeaterActive = (new BlockRedstoneRepeater(94, true)).setHardness(0.0F).setLightValue(10.0F / 16.0F).setStepSound(soundWoodFootstep).setBlockName("diode").disableStats().disableNeighborNotifyOnMetadataChange();
 	public static final Block lockedChest = (new BlockLockedChest(95)).setHardness(0.0F).setLightValue(1.0F).setStepSound(soundWoodFootstep).setBlockName("lockedchest").disableStats().setTickOnLoad(true).disableNeighborNotifyOnMetadataChange();
 	public static final Block trapdoor = (new BlockTrapDoor(96, Material.wood)).setHardness(3.0F).setStepSound(soundWoodFootstep).setBlockName("trapdoor").disableStats().disableNeighborNotifyOnMetadataChange();
-	public static final Block oreRuby = (new BlockOreStorage(97, 196)).setHardness(3.0F).setResistance(5.0F).setStepSound(soundStoneFootstep).setBlockName("oreRuby");
-	public static final Block blockRuby = (new BlockRuby(98, 114)).setHardness(5.0F).setResistance(10.0F).setStepSound(soundMetalFootstep).setBlockName("blockRuby");
+	public static final Block oreRuby = (new BlockOre(97, 196)).setHardness(3.0F).setResistance(5.0F).setStepSound(soundStoneFootstep).setBlockName("oreRuby");
+	public static final Block blockRuby = (new BlockOreStorage(98, 114)).setHardness(5.0F).setResistance(10.0F).setStepSound(soundMetalFootstep).setBlockName("blockRuby");
 	public int blockIndexInTexture;
 	public final int blockID;
 	protected float blockHardness;
