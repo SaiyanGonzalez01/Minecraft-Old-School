6e68b55d66373fb72cb478d85e378dd5fd378467
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-01-31 16:33:00 -0500

    Update ItemBow.java

--- src/main/java/net/minecraft/src/ItemBow.java
+++ src/main/java/net/minecraft/src/ItemBow.java
@@ -4,6 +4,7 @@ public class ItemBow extends Item {
 	public ItemBow(int var1) {
 		super(var1);
 		this.maxStackSize = 1;
+		this.setMaxDamage(var3.getMaxUses());
 	}
 
 	public ItemStack onItemRightClick(ItemStack var1, World var2, EntityPlayer var3) {
