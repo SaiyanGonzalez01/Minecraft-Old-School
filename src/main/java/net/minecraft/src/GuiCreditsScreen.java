1a46fef128fae9c4b5915f422c330af44a857a92
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-02-15 23:41:51 -0500

    Create GuiCreditsScreen.java

---
+++ src/main/java/net/minecraft/src/GuiCreditsScreen.java
@@ -0,0 +1,39 @@
+package net.minecraft.src;
+
+public class GuiErrorScreen extends GuiScreen {
+	private int field_28098_a = 0;
+
+	public void updateScreen() {
+		++this.field_28098_a;
+	}
+
+	public void initGui() {
+    
+      this.controlList.add(new GuiButton(301, this.width / 2 - 100, this.height / 6 + 168, var1.translateKey("gui.done")));
+    
+	}
+
+	protected void actionPerformed(GuiButton var1) {
+    
+      if(var1.id == 200) {
+				  this.mc.gameSettings.saveOptions();
+				  this.mc.displayGuiScreen(this.parentScreen);
+
+	}
+
+	protected void keyTyped(char var1, int var2) {
+	}
+
+	public void drawScreen(int var1, int var2, float var3) {
+		this.drawDefaultBackground();
+		this.drawCenteredString(this.fontRenderer, "Oi this is testing!", this.width / 2, this.height / 4 - 60 + 20, 16777215);
+		this.drawString(this.fontRenderer, "Minecraft Old-School has run out of memory.", this.width / 2 - 140, this.height / 4 - 60 + 60 + 0, 10526880);
+		this.drawString(this.fontRenderer, "This could be caused by a bug in the game or by the", this.width / 2 - 140, this.height / 4 - 60 + 60 + 18, 10526880);
+		this.drawString(this.fontRenderer, "Java Virtual Machine not being allocated enough", this.width / 2 - 140, this.height / 4 - 60 + 60 + 27, 10526880);
+		this.drawString(this.fontRenderer, "memory. If you are playing in a web browser, try", this.width / 2 - 140, this.height / 4 - 60 + 60 + 36, 10526880);
+		this.drawString(this.fontRenderer, "downloading the game and playing it offline.", this.width / 2 - 140, this.height / 4 - 60 + 60 + 45, 10526880);
+		this.drawString(this.fontRenderer, "To prevent your world from corrupting, your current game has quit.", this.width / 2 - 140, this.height / 4 - 60 + 60 + 63, 10526880);
+		this.drawString(this.fontRenderer, "Please restart the game.", this.width / 2 - 140, this.height / 4 - 60 + 60 + 81, 10526880);
+		super.drawScreen(var1, var2, var3);
+	}
+}
