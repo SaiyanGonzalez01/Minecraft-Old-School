49bb151038226c63bd309d8494f23575f73d88f2
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-02-27 20:36:42 -0500

    Update Item.java

--- src/main/java/net/minecraft/src/Item.java
+++ src/main/java/net/minecraft/src/Item.java
@@ -118,6 +118,7 @@ public class Item {
 	public static Item plateStone = (new ItemArmor(114, 2, 2, 1)).setIconCoord(0, 14).setItemName("chestplateStone");
 	public static Item sawdust = (new Item(115)).setIconCoord(13, 3).setItemName("sawdust");
 	public static Item bark = (new Item(116)).setIconCoord(14, 3).setItemName("bark");
+	public static Item saltI = (new Item(117)).setIconCoord(12, 6).setItemName("saltI");
 	public static Item record13 = (new ItemRecord(2000, "13")).setIconCoord(0, 15).setItemName("record");
 	public static Item recordCat = (new ItemRecord(2001, "cat")).setIconCoord(1, 15).setItemName("record");
 	public final int shiftedIndex;
