680f83674b27d23a230de5172edb115ac099ff33
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-03-21 08:40:19 -0400

    Add files via upload

---
+++ MakeOfflineDownload.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -cp "desktopRuntime/MakeOfflineDownload.jar:desktopRuntime/CompileEPK.jar" net.lax1dude.eaglercraft.v1_8.buildtools.workspace.MakeOfflineDownload "javascript/OfflineDownloadTemplate.txt" "javascript/classes.js" "javascript/assets.epk" "javascript/Project_Classica_US.html" "javascript/Project_Classica_Inter.html" "javascript/lang"
