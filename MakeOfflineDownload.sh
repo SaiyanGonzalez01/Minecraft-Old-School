d96ffe2070b883d97efc6d4d73973ac32f74397f
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-03-21 08:43:56 -0400

    Update MakeOfflineDownload.sh

--- MakeOfflineDownload.sh
+++ MakeOfflineDownload.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp "desktopRuntime/MakeOfflineDownload.jar:desktopRuntime/CompileEPK.jar" net.lax1dude.eaglercraft.v1_8.buildtools.workspace.MakeOfflineDownload "javascript/OfflineDownloadTemplate.txt" "javascript/classes.js" "javascript/assets.epk" "javascript/Project_Classica_US.html" "javascript/Project_Classica_Inter.html" "javascript/lang"
+java -cp "jars/MakeOfflineDownload.jar:jars/CompilePackage.jar" net.lax1dude.eaglercraft.v1_8.buildtools.workspace.MakeOfflineDownload "web/OfflineDownloadTemplate.txt" "web/js/app.js" "web/js/app.js.map" "web/resources.mc" "web/Minecraft_Old-School_US.html" "web/Minecraft_Old-School_Inter.html" "web/lang"
