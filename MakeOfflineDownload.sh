7bcf86e5ed0cd569085032404bed3c39eddffbed
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-03-21 09:38:52 -0400

    Delete MakeOfflineDownload.sh

--- MakeOfflineDownload.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp "jars/MakeOfflineDownload.jar:jars/CompilePackage.jar" net.lax1dude.eaglercraft.v1_8.buildtools.workspace.MakeOfflineDownload "web/OfflineDownloadTemplate.txt" "web/js/app.js" "web/js/app.js.map" "web/resources.mc" "web/Minecraft_Old-School_US.html" "web/Minecraft_Old-School_Inter.html" "web/lang"
