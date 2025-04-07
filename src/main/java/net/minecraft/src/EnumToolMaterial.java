f8df6d4e699c369bec7bd49eeeef7ec19171ccb9
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-04-07 10:53:37 -0400

    Update EnumToolMaterial.java

--- src/main/java/net/minecraft/src/EnumToolMaterial.java
+++ src/main/java/net/minecraft/src/EnumToolMaterial.java
@@ -4,8 +4,11 @@ public enum EnumToolMaterial {
 	WOOD(0, 59, 2.0F, 0),
 	STONE(1, 131, 4.0F, 1),
 	IRON(2, 250, 6.0F, 2),
+	GREATIRON(2, 300, 6.0F, 3),
 	EMERALD(3, 1561, 8.0F, 3),
+	GREATEMERALD(3, 1651, 8.0F, 4),
 	GOLD(0, 32, 12.0F, 0),
+	GREATGOLD(0, 128, 12.0F, 1),
 	OBSIDIAN(4, 2561, 12.0F, 4);
 
 	private final int harvestLevel;
