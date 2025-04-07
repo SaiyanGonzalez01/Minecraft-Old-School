ce729f52b7d598f5aa9ec0f3c547076dd3319a1e
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-04-07 10:30:10 -0400

    Update Block.java

--- src/main/java/net/minecraft/src/Block.java
+++ src/main/java/net/minecraft/src/Block.java
@@ -127,7 +127,6 @@ public class Block {
 	public static final BlockFlower plantCyan = (BlockFlower)(new BlockFlower(105, 111)).setHardness(0.0F).setStepSound(soundGrassFootstep).setBlockName("cyanRose");
 	public static final Block brickStone = (new Block(106, 85, Material.rock)).setHardness(1.5F).setResistance(10.0F).setStepSound(soundStoneFootstep).setBlockName("brickStone");
 	public static final Block pillarStone = (new Block(107, 126, Material.rock)).setHardness(1.5F).setResistance(10.0F).setStepSound(soundStoneFootstep).setBlockName("pillarStone");
-	public static final Block mud = (BlockSoulSand)(new BlockSoulSand(108, 125)).setHardness(0.5F).setStepSound(soundGravelFootstep).setBlockName("mud");
 	public int blockIndexInTexture;
 	public final int blockID;
 	protected float blockHardness;
