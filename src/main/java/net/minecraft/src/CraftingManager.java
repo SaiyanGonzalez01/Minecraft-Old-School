676c51165ca5f449e0feb8572c9496d42a7a9772
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-04-08 18:51:24 -0400

    Update CraftingManager.java

--- src/main/java/net/minecraft/src/CraftingManager.java
+++ src/main/java/net/minecraft/src/CraftingManager.java
@@ -91,6 +91,7 @@ public class CraftingManager {
 		this.addRecipe(new ItemStack(Item.cookieSugar, 8), new Object[]{"#X#", Character.valueOf('#'), Item.wheat, Character.valueOf('X'), Item.sugar});
 		this.addRecipe(new ItemStack(Block.brickStone, 4), new Object[]{"##", "##", Character.valueOf('#'), Block.stone});
 		this.addRecipe(new ItemStack(Block.pillarStone, 3), new Object[]{"#", "#", "#", Character.valueOf('#'), Block.stone});
+		this.addRecipe(new ItemStack(Item.greatswordIron, 1), new Object[]{"#", "X", "Y", Character.valueOf('#'), Item.swordSteel, Character.valueOf("X"), Item.swordpaper, Character.valueOf("Y"), Item.ruby});
 		
 		Collections.sort(this.recipes, new RecipeSorter(this));
 		System.out.println(this.recipes.size() + " recipes");
