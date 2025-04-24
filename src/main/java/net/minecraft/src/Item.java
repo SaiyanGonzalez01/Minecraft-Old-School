8f79bcf1fc8fa4ade8008b193da89226930507f1
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-04-24 19:06:02 -0400

    Update Item.java

--- src/main/java/net/minecraft/src/Item.java
+++ src/main/java/net/minecraft/src/Item.java
@@ -130,6 +130,7 @@ public class Item {
 	public static Item potato = (new ItemSeeds(128, Block.potatoCrops.blockID)).setIconCoord(12, 10).setItemName("potato");
 	public static Item ironNugget = (new Item(129)).setIconCoord(6, 6).setItemName("itemNugget");
 	public static Item chainlink = (new Item(130)).setIconCoord(8, 8).setItemName("chainlink");
+	public static Item potatoCooked = (new ItemFood(131, 5, true)).setIconCoord(11, 10).setItemName("potatoCooked");
 	public static Item record13 = (new ItemRecord(2000, "13")).setIconCoord(0, 15).setItemName("record");
 	public static Item recordCat = (new ItemRecord(2001, "cat")).setIconCoord(1, 15).setItemName("record");
 	public final int shiftedIndex;
