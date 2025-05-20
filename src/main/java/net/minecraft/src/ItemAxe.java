f5242277c11dc9cf2f770f838312274594c17c1c
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-05-19 20:24:25 -0400

    Update ItemAxe.java

--- src/main/java/net/minecraft/src/ItemAxe.java
+++ src/main/java/net/minecraft/src/ItemAxe.java
@@ -1,7 +1,7 @@
 package net.minecraft.src;
 
 public class ItemAxe extends ItemTool {
-	private static Block[] blocksEffectiveAgainst = new Block[]{Block.planks, Block.bookShelf, Block.wood, Block.chest, Block.workbench, Block.stairCompactPlanks, Block.jukebox, Block.doorWood, Block.fence, Block.pressurePlatePlanks, Block.trapdoor};
+	private static Block[] blocksEffectiveAgainst = new Block[]{Block.planks, Block.bookShelf, Block.wood, Block.chest, Block.workbench, Block.stairCompactPlanks, Block.jukebox, Block.doorWood, Block.fence, Block.pressurePlatePlanks, Block.trapdoor, Block.planksColored};
 
 	protected ItemAxe(int var1, EnumToolMaterial var2) {
 		super(var1, 3, var2, blocksEffectiveAgainst);
