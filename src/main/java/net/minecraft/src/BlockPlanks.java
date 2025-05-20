18f09bae91f1a852d7e63ba0bc3137a7f4b84dc4
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-05-20 09:26:24 -0400

    Update BlockPlanks.java

--- src/main/java/net/minecraft/src/BlockPlanks.java
+++ src/main/java/net/minecraft/src/BlockPlanks.java
@@ -2,7 +2,7 @@ package net.minecraft.src;
 
 public class BlockPlanks extends Block {
 	public BlockPlanks() {
-		super(127, 153, Material.wood);
+		super(127, 9, Material.wood);
 	}
 
 	public int getBlockTextureFromSideAndMetadata(int var1, int var2) {
