7cd147cb2577379bde4c090a4922d814190546bd
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-05-26 14:36:19 -0400

    Create BlockNetherRice.java

---
+++ src/main/java/net/minecraft/src/BlockNetherRice.java
@@ -0,0 +1,13 @@
+package net.minecraft.src;
+
+public class BlockNetherRice extends BlockFlower {
+	protected BlockNetherRice(int var1, int var2) {
+		super(var1, var2);
+		float var3 = 0.4F;
+		this.setBlockBounds(0.5F - var3, 0.0F, 0.5F - var3, 0.5F + var3, 0.8F, 0.5F + var3);
+	}
+
+	public int idDropped(int var1, Random var2) {
+		return var2.nextInt(8) == 0 ? Item.netherrice.shiftedIndex : 2;
+	}
+}
