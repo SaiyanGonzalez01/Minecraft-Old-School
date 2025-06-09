0e446b30ac5d6cccf134939bdc72c7c5734e04f5
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-06-08 21:15:26 -0400

    Update ItemCannon.java

--- src/main/java/net/minecraft/src/ItemCannon.java
+++ src/main/java/net/minecraft/src/ItemCannon.java
@@ -7,7 +7,7 @@ public class ItemCannon extends Item {
 	}
 
 	public ItemStack onItemRightClick(ItemStack var1, World var2, EntityPlayer var3) {
-		if(var3.inventory.consumeInventoryItem(Item.arrow.shiftedIndex)) {
+		if(var3.inventory.consumeInventoryItem(Item.cannonball.shiftedIndex)) {
 			if(!var2.multiplayerWorld) {
 				var2.playSoundAtEntity(var3, "random.bow", 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 0.8F));
 				var2.entityJoinedWorld(new EntityCannonball(var2, var3));
