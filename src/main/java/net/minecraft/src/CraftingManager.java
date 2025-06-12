db48957acb67acc58ae79b3a7a333e8899c22853
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-06-12 09:01:27 -0400

    Update CraftingManager.java

--- src/main/java/net/minecraft/src/CraftingManager.java
+++ src/main/java/net/minecraft/src/CraftingManager.java
@@ -99,7 +99,7 @@ public class CraftingManager {
 		this.addRecipe(new ItemStack(Item.bowlRice, 1), new Object[]{" # ", "#X#", " # ", Character.valueOf('#'), Item.netherrice, Character.valueOf('X'), Item.bowlEmpty});
 		this.addRecipe(new ItemStack(Item.bowlRicePork, 1), new Object[]{"#X", Character.valueOf('#'), Item.porkCooked, Character.valueOf('X'), Item.bowlRice});
 		this.addRecipe(new ItemStack(Item.bowlRiceEgg, 1), new Object[]{"#X", Character.valueOf('#'), Item.eggCooked, Character.valueOf('X'), Item.bowlRice});
-		this.addRecipe(new ItemStack(Item.cannonball, 1), new Object[]{"X#X", "#O#", "X#X", Character.valueOf('#'), Item.gunpowder, Character.valueOf('X'), Block.stone, Character.valueOf('O'), Item.redstone});
+		this.addRecipe(new ItemStack(Item.cannonball, 1), new Object[]{"X#X", "#O#", "X#X", Character.valueOf('#'), Item.gunpowder, Character.valueOf('X'), Item.ingotIron, Character.valueOf('O'), Item.redstone});
 		this.addRecipe(new ItemStack(Item.cannon, 1), new Object[]{"XX#", " ZY", Character.valueOf('#'), Block.planks, Character.valueOf('X'), Item.ingotIron, Character.valueOf('Y'), Item.stick, Character.valueOf('Z'), Item.redstone});
 		
 		Collections.sort(this.recipes, new RecipeSorter(this));
