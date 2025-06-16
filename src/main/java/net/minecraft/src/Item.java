d3ab332a3e9f2274996f53960aa0d246c8ca0b88
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-06-16 08:59:08 -0400

    Update Item.java

--- src/main/java/net/minecraft/src/Item.java
+++ src/main/java/net/minecraft/src/Item.java
@@ -139,6 +139,7 @@ public class Item {
 	public static Item bowlRiceEgg = (new ItemSoup(137, 7)).setIconCoord(9, 11).setItemName("eggBowl");
 	public static Item cannon = (new ItemCannon(138)).setIconCoord(6, 9).setItemName("cannon").setFull3D();
 	public static Item cannonball = (new Item(139)).setIconCoord(6, 10).setItemName("cannonball");
+	public static Item goldNugget = (new Item(140)).setIconCoord(6, 7).setItemName("goldNugget");
 	public static Item record13 = (new ItemRecord(2000, "13")).setIconCoord(0, 15).setItemName("record");
 	public static Item recordCat = (new ItemRecord(2001, "cat")).setIconCoord(1, 15).setItemName("record");
 	public final int shiftedIndex;
