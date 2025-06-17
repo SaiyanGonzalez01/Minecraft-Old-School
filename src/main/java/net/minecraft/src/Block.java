df7b59893a032348f7d452bbcc944cb607b792f0
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-06-17 18:57:31 -0400

    Update Block.java

--- src/main/java/net/minecraft/src/Block.java
+++ src/main/java/net/minecraft/src/Block.java
@@ -133,7 +133,7 @@ public class Block {
 	public static final BlockNetherRice netherriceWild = (BlockNetherRice)(new BlockNetherRice(113, 183)).setHardness(0.0F).setStepSound(soundGrassFootstep).setBlockName("netherriceWild");
 	public static final Block netherriceCrops = (new BlockCropsR(114, 226)).setHardness(0.0F).setStepSound(soundGrassFootstep).setBlockName("crops").disableStats().disableNeighborNotifyOnMetadataChange();
 	public static final Block blackstone = (new Block(115, 182, Material.rock)).setHardness(1.5F).setResistance(10.0F).setStepSound(soundStoneFootstep).setBlockName("blackstone");
-	public static final Block oreGoldNether = (new BlockNetherGoldOre(116, 153)).setHardness(0.5F).setStepSound(soundStoneFootstep).setBlockName("netherGold");
+	public static final Block oreGoldNether = (new BlockNetherGoldOre(116, 153, Material.rock)).setHardness(0.5F).setStepSound(soundStoneFootstep).setBlockName("netherGold");
 	public static final Block planksTreated = (new Block(117, 8, Material.wood)).setHardness(2.0F).setResistance(5.0F).setStepSound(soundWoodFootstep).setBlockName("woodTreated").disableNeighborNotifyOnMetadataChange();
 	public int blockIndexInTexture;
 	public final int blockID;
