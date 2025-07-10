ac47949368b384731c6ada355e42dc1d6b924df6
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-07-10 00:34:50 -0400

    Update ItemSoup.java

--- src/main/java/net/minecraft/src/ItemSoup.java
+++ src/main/java/net/minecraft/src/ItemSoup.java
@@ -7,6 +7,7 @@ public class ItemSoup extends ItemFood {
 
 	public ItemStack onItemRightClick(ItemStack var1, World var2, EntityPlayer var3) {
 		super.onItemRightClick(var1, var2, var3);
+		var2.playSoundAtEntity(var3, "random.slurp", 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 0.8F));
 		return new ItemStack(Item.bowlEmpty);
 	}
 }
