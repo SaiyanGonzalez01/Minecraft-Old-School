1ca6764be0a577db88bda0e211d7f56ccf47ecb6
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-07-11 23:30:33 -0400

    Update BiomeGenTundra.java

--- src/main/java/net/minecraft/src/BiomeGenTundra.java
+++ src/main/java/net/minecraft/src/BiomeGenTundra.java
@@ -2,7 +2,7 @@ package net.minecraft.src;
 
 public class BiomeGenTundra extends BiomeGenBase {
   public BiomeGenTundra() {
-		this.spawnableCreatureList.add(new SpawnListEntry(EntityIceSlime.class, 1));
+		this.spawnableMonsterList.add(new SpawnListEntry(EntityIceSlime.class, 8));
 	}
 
 }
