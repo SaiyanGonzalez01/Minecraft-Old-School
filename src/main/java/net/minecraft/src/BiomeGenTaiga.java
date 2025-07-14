dac55de2d6d2b53e12f31b395fe7102fd9a6fe50
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-07-13 23:59:56 -0400

    Update BiomeGenTaiga.java

--- src/main/java/net/minecraft/src/BiomeGenTaiga.java
+++ src/main/java/net/minecraft/src/BiomeGenTaiga.java
@@ -2,8 +2,10 @@ package net.minecraft.src;
 
 public class BiomeGenTaiga extends BiomeGenBase {
 	public BiomeGenTaiga() {
+		this.spawnableMonsterList.clear();
 		this.spawnableCreatureList.add(new SpawnListEntry(EntityWolf.class, 2));
 		this.spawnableMonsterList.add(new SpawnListEntry(EntityIceSlime.class, 8));
+		this.spawnableMonsterList.add(new SpawnListEntry(EntityIceSkeleton.class, 6));
 	}
 
 	public WorldGenerator getRandomWorldGenForTrees(Random var1) {
