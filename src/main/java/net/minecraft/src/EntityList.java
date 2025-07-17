8583d2761ecef1f71e9548ec26c7afcbc7cd3101
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-07-17 17:53:56 -0400

    Update EntityList.java

--- src/main/java/net/minecraft/src/EntityList.java
+++ src/main/java/net/minecraft/src/EntityList.java
@@ -107,6 +107,7 @@ public class EntityList {
 		addMapping(EntityNetherSkeleton.class, "CharredSkeleton", 58);
 		addMapping(EntityIceSlime.class, "IceSlime", 59);
 		addMapping(EntityIceSkeleton.class, "IceSkeleton", 60);
+		addMapping(EntityMagmaSlime.class, "MagmaSlime", 61);
 		addMapping(EntityPig.class, "Pig", 90);
 		addMapping(EntitySheep.class, "Sheep", 91);
 		addMapping(EntityCow.class, "Cow", 92);
