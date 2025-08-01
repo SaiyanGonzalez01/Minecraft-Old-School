ba8c4ed552dc51bbd484fbbd2ab3e9005631359a
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-07-31 23:58:49 -0400

    Update Item.java

--- src/main/java/net/minecraft/src/Item.java
+++ src/main/java/net/minecraft/src/Item.java
@@ -166,7 +166,7 @@ public class Item {
 	public static Item slimeballFrozen = (new Item(164)).setIconCoord(13, 7).setItemName("slimeballFrozen");
 	public static Item iceBall = (new ItemIceball(165)).setIconCoord(13, 6).setItemName("iceball");
 	public static Item runeEarth = (new Item(166)).setIconCoord(6, 12).setItemName("runeEarth");
-	public static Item swordLesserObsidian = (new ItemSword(167, EnumToolMaterial.OBSIDIAN)).setIconCoord(0, 9).setItemName("swordObsidian");
+	public static Item swordLesserObsidian = (new ItemSword(167, EnumToolMaterial.OBSIDIAN)).setIconCoord(0, 14).setItemName("swordLesserObsidian");
 	public static Item shovelObsidian = (new ItemSpade(168, EnumToolMaterial.OBSIDIAN)).setIconCoord(0, 10).setItemName("shovelObsidian");
 	public static Item pickaxeObsidian = (new ItemPickaxe(169, EnumToolMaterial.OBSIDIAN)).setIconCoord(0, 11).setItemName("pickaxeObsidian");
 	public static Item axeObsidian = (new ItemAxe(170, EnumToolMaterial.OBSIDIAN)).setIconCoord(0, 12).setItemName("hatchetObsidian");
