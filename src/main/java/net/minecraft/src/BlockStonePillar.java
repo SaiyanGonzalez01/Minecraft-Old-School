3ba5d315d7835f4ffa231fdb991590820bf07d0e
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-08-19 00:05:42 -0400

    Create BlockStonePillar.java

---
+++ src/main/java/net/minecraft/src/BlockStonePillar.java
@@ -0,0 +1,16 @@
+package net.minecraft.src;
+
+public class BlockPillar extends Block {
+	public BlockPillar(int var1, int var2) {
+		super(var1, var2, Material.rock);
+    this.blockIndexInTexture = 126;
+	}
+
+	public int idDropped(int var1, Random var2) {
+		return Block.cobblestone.blockID;
+	}
+  
+  public int getBlockTextureFromSide(int var1) {
+		return var1 == 1 ? 127 : (var1 == 0 ? 127 : 126);
+	}
+}
