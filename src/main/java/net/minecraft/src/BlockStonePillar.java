8d2151c3bb674b29c5ae90f1acbdec3d33c22a36
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-08-19 22:55:49 -0400

    Update BlockStonePillar.java

--- src/main/java/net/minecraft/src/BlockStonePillar.java
+++ src/main/java/net/minecraft/src/BlockStonePillar.java
@@ -6,10 +6,6 @@ public class BlockStonePillar extends Block {
     this.blockIndexInTexture = 126;
 	}
 
-	public int idDropped(int var1, Random var2) {
-		return Block.cobblestone.blockID;
-	}
-  
   public int getBlockTextureFromSide(int var1) {
 		return var1 == 1 ? 127 : (var1 == 0 ? 127 : 126);
 	}
