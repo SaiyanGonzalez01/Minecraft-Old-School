772850fd6e734e3f851f299f924cc9a99ffa9bde
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-08-21 23:02:29 -0400

    Update Block.java

--- src/main/java/net/minecraft/src/Block.java
+++ src/main/java/net/minecraft/src/Block.java
@@ -136,7 +136,6 @@ public class Block {
 	public static final Block oreGoldNether = (new BlockNetherGoldOre(116, 153, Material.rock)).setHardness(0.5F).setStepSound(soundStoneFootstep).setBlockName("netherGold");
 	public static final Block planksTreated = (new Block(117, 8, Material.wood)).setHardness(2.0F).setResistance(5.0F).setStepSound(soundWoodFootstep).setBlockName("woodTreated").disableNeighborNotifyOnMetadataChange();
 	public static final Block brickMoss = (new Block(118, 172, Material.rock)).setHardness(2.0F).setResistance(10.0F).setStepSound(soundStoneFootstep).setBlockName("brickMoss");
-	public static final BlockSkyPortal portalSky = (BlockSkyPortal)(new BlockSkyPortal(119, 14)).setHardness(-1.0F).setStepSound(soundGlassFootstep).setLightValue(12.0F / 16.0F).setBlockName("portalSky").disableStats();
 	public static final Block cobbledGranite = (new Block(120, 101, Material.rock)).setHardness(2.0F).setResistance(10.0F).setStepSound(soundStoneFootstep).setBlockName("graniteCobbled");
 	public static final Block plankedCobblestone = (new BlockPlankedStone(121, 173)).setHardness(2.5F).setResistance(9.0F).setStepSound(soundWoodFootstep).setBlockName("plankedCobblestone");
 	public static final Block plankedGranite = (new BlockPlankedGranite(122, 174)).setHardness(2.5F).setResistance(9.0F).setStepSound(soundWoodFootstep).setBlockName("plankedGranite");
