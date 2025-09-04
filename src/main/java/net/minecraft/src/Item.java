4c1aaa74cc55158344e9dafbb29c9b7853ae7050
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-09-03 20:23:20 -0400

    Update Item.java

--- src/main/java/net/minecraft/src/Item.java
+++ src/main/java/net/minecraft/src/Item.java
@@ -180,6 +180,7 @@ public class Item {
 	public static Item arrowIron = (new Item(178)).setIconCoord(5, 10).setItemName("arrowIron");
 	public static Item arrowGold = (new Item(179)).setIconCoord(5, 11).setItemName("arrowGold");
 	public static Item bowIron = (new ItemIronBow(180)).setIconCoord(5, 8).setItemName("bowIron").setFull3D();
+	public static Item bowGold = (new ItemGoldBow(181)).setIconCoord(5, 9).setItemName("bowGold").setFull3D();
 	public static Item record13 = (new ItemRecord(2000, "13")).setIconCoord(0, 15).setItemName("record");
 	public static Item recordCat = (new ItemRecord(2001, "cat")).setIconCoord(1, 15).setItemName("record");
 	public final int shiftedIndex;
