26b8f6083c67c2498a6e5a1e4284972fee2b18e9
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-09-19 18:52:36 -0400

    Update ItemDrink.java

--- src/main/java/net/minecraft/src/ItemDrink.java
+++ src/main/java/net/minecraft/src/ItemDrink.java
@@ -1,13 +1,23 @@
 package net.minecraft.src;
 
-public class ItemDrink extends ItemFood {
-	public ItemDrink(int var1, int var2) {
-		super(var1, var2, false);
+public class ItemDrink extends Item {
+	public int healAmount;
+
+	public ItemDrink(int var1, int var2, boolean var3) {
+		super(var1);
+		this.healAmount = var2;
+		this.maxStackSize = 1;
 	}
 
 	public ItemStack onItemRightClick(ItemStack var1, World var2, EntityPlayer var3) {
-		super.onItemRightClick(var1, var2, var3);
+		--var1.stackSize;
 		var2.playSoundAtEntity(var3, "random.drink", 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 0.8F));
+		var3.heal(this.healAmount);
 		return new ItemStack(Item.bottle);
+		return var1;
+	}
+
+	public int getHealAmount() {
+		return this.healAmount;
 	}
 }
