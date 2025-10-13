1fb1f4c1ba297b7b469e68814f66e51cf53eb65e
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-10-12 20:59:49 -0400

    Update and rename ItemScythe.java to ItemSickle.java

--- src/main/java/net/minecraft/src/ItemSickle.java
+++ src/main/java/net/minecraft/src/ItemSickle.java
@@ -1,9 +1,9 @@
 package net.minecraft.src;
 
-public class ItemScythe extends ItemTool {
+public class ItemSickle extends ItemTool {
 	private static Block[] blocksEffectiveAgainst = new Block[]{Block.crops, Block.potatoCrops, Block.netherriceWild, Block.netherriceCrops, Block.leaves};
 
-	protected ItemScythe(int var1, EnumToolMaterial var2) {
+	protected ItemSickle(int var1, EnumToolMaterial var2) {
 		super(var1, 3, var2, blocksEffectiveAgainst);
 	}
 }
