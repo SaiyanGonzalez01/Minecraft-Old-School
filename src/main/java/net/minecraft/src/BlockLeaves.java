c2e8340d28e49996e28acfd5c0fe2b434f16706d
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-10-13 11:26:27 -0400

    Update BlockLeaves.java

--- src/main/java/net/minecraft/src/BlockLeaves.java
+++ src/main/java/net/minecraft/src/BlockLeaves.java
@@ -162,7 +162,6 @@ public class BlockLeaves extends BlockLeavesBase {
 				!var1.multiplayerWorld && var2.getCurrentEquippedItem() != null && var2.getCurrentEquippedItem().itemID == Item.sickleWood.shiftedIndex
 			) {
 				var2.addStat(StatList.mineBlockStatArray[this.blockID], 1);
-				return var1.nextInt(20) == 0 ? 1 : 0;
 				this.dropBlockAsItem_do(var1, var3, var4, var5, new ItemStack(Item.appleRed, 1, 0));
 			} else {
 				super.harvestBlock(var1, var2, var3, var4, var5, var6);
