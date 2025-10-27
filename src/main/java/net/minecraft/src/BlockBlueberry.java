8a49b35a8cdb55c918caba45ff2c57f87b1f93d3
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-10-26 21:38:37 -0400

    Create BlockBlueberry.java

---
+++ src/main/java/net/minecraft/src/BlockBlueberry.java
@@ -0,0 +1,13 @@
+package net.minecraft.src;
+
+public class BlockBlueberry extends BlockFlower {
+	protected BlockBlueberry(int var1, int var2) {
+		super(var1, var2);
+		float var3 = 0.4F;
+		this.setBlockBounds(0.5F - var3, 0.0F, 0.5F - var3, 0.5F + var3, 0.8F, 0.5F + var3);
+	}
+
+	public int idDropped(int var1, Random var2) {
+		return var2.nextInt(4) == 0 ? Item.blueberry.shiftedIndex : 2;
+	}
+}
