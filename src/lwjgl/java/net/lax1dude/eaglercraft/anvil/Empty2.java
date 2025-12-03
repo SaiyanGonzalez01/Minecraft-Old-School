85805f3c6b6275b1aee11f8a839d7be9d938892d
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-12-02 19:27:58 -0500

    Delete src directory

--- src/lwjgl/java/net/lax1dude/eaglercraft/anvil/Empty2.java
+++
@@ -1,8 +0,0 @@
-package net.lax1dude.eaglercraft.anvil;
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) braces deadcode 
-
-class Empty2 {
-}
